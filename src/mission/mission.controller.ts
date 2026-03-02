f8d7ee04f6e0b35d138cde28ce8fb35bc90ac648
JDlnwza007 <68010147@kmitl.ac.th>
2026-03-02 18:57:38 +0700

    smksksfsdsd

---
+++ src/mission/mission.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { MissionService } from './mission.service';
+
+@Controller('missions')
+export class MissionController {
+  
+  constructor(private readonly missionService: MissionService) {}
+
+  @Get('summary')
+  getSummary() {
+    return this.missionService.getSummary();
+  @Get()
+  findAll() {
+    return this.missionService.findAll();
+  }
+  }
+}
---
+++ src/mission/mission.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MissionService {
+  private readonly missions = [
+    { id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
+    { id: 2, codename: 'SILENT_SNAKE', status: 'COMPLETED' },
+    { id: 3, codename: 'RED_DAWN', status: 'FAILED' },
+    { id: 4, codename: 'BLACKOUT', status: 'ACTIVE' },
+    { id: 5, codename: 'ECHO_FALLS', status: 'COMPLETED' },
+    { id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' }
+  ];
+
+  getSummary() {
+    let summary: { [key: string]: number } = {};
+
+    for (let i = 0; i < this.missions.length; i++) {
+      
+      let currentMission = this.missions[i];
+      let currentStatus = currentMission.status;
+
+      if (summary[currentStatus] === undefined) {
+        summary[currentStatus] = 1;
+      } 
+      else {
+        summary[currentStatus] = summary[currentStatus] + 1;
+      }
+    }
+    return summary;
+  }
+
+  findAll() {
+
+    const rawData = fs.readFileSync('data/missions.json', 'utf-8');
+    const missions: IMission[] = JSON.parse(rawData);
+    const result = missions.map((mission) => {
+
+      let durationDays = -1;
+      if (mission.endDate !== null) {
+        const startMillis = new Date(mission.startDate).getTime();
+        const endMillis = new Date(mission.endDate).getTime();
+        const diffInMs = endMillis - startMillis;
+        const msInOneDay = 1000 * 60 * 60 * 24;
+        durationDays = diffInMs / msInOneDay;
+      }
+
+     mission.durationDays = durationDays;
+      
+      return mission; 
+    });
+    return result;
+  }
+}
